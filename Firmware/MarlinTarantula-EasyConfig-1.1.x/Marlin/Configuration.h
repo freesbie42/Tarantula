31c84f1403485912abaf9b7ca000b010d48032fd
mato <mato@bioadmins-MBP.fritz.box>
2018-01-12 15:58:05 +0100

    adjust extruder over z

--- Firmware/MarlinTarantula-EasyConfig-1.1.x/Marlin/Configuration.h
+++ Firmware/MarlinTarantula-EasyConfig-1.1.x/Marlin/Configuration.h
@@ -953,7 +953,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER SENSOR_RIGHT - SENSOR_LEFT  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER SENSOR_BEHIND - SENSOR_FRONT // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.20   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 13500
